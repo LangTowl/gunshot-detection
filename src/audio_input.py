8285ed3f41afb3e854d74eb81620e526cdebe27d
Lang Towl <langtowl@gmail.com>
2025-02-23 10:32:28 -0500

    Implements downscaling factor to reduce audio volume sent to the spec generator

--- src/audio_input.py
+++ src/audio_input.py
@@ -6,7 +6,7 @@ from multiprocessing import Value
 volume_queue = Queue()          # Queue is accessed by volume rendered
 spectrogram_queue = Queue()     # Queue is used to generate spectrogram
 
-def record_audio(stop_event, chunk_duration = 0.01, sample_rate = 16000, channels = 1):
+def record_audio(stop_event, chunk_duration = 0.01, sample_rate = 16000, channels = 1, downscale_factor = 0.35):
 
     # Determine sample
     num_samples = int(sample_rate * chunk_duration)
@@ -37,6 +37,9 @@ def record_audio(stop_event, chunk_duration = 0.01, sample_rate = 16000, channel
             rms = np.sqrt(np.mean(np.square(audio_segment)))
             volume_level = min(1.0, rms * 10)
             volume_queue.put(volume_level)
+
+            # Downscale the audio segment
+            audio_segment *= downscale_factor
 
             # Append audio buffer to spectrogram queue once it accumulates 2 seconds of audio
             audio_buffer_1.append(audio_segment)
