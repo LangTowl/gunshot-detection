b7088625d9fde7b36aeb95b1b34d0a00870e0f45
Lang Towl <langtowl@gmail.com>
2025-04-15 12:56:01 -0400

    Update downscale factor

--- src/audio_input.py
+++ src/audio_input.py
@@ -6,7 +6,7 @@ volume_queue = Queue()          # Queue is accessed by volume rendered
 spectrogram_queue = Queue()     # Queue is used to generate spectrogram
 renderer_queue = Queue()        # Queue is use to render spectrogram
 
-def record_audio(stop_event, chunk_duration = 0.01, sample_rate = 16000, channels = 1, downscale_factor = 0.50):
+def record_audio(stop_event, chunk_duration = 0.01, sample_rate = 16000, channels = 1, downscale_factor = 0.25):
     # Debug
     print("Initializing audio input pipeline...\n")
 
